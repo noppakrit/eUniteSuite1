7621bfbc6c42ec2fde8e6076bd1122eb3a32315e
noppakrit <noppakrit.t@windowslive.com>
2016-09-07 23:23:40 +0700

    init

--- README.md
+++
@@ -1,1 +0,0 @@
-# eUniteSuite1
---
+++ src/test/java/EUniteSuite1.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Created by noppakrit on 07/09/2016.
+ */
+public class EUniteSuite1 {
+
+    WebDriver driver;
+
+    @Test
+    public void loginShouldBeSuccess() {
+
+    }
+}
